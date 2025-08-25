373c9fa2813a944793daf8debe3a1df1a03e842a
MOUNIKA18082004 <mouni73588@gmail.com>
2025-08-25 10:58:49 +0530

    get_post_put_delete

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request
 from db import stores
+import uuid
 
 app = Flask(__name__)
 
@@ -10,14 +11,24 @@ def all_stores():
 @app.post("/add_stores")
 def add_stores():
     json_body = request.get_json()
-    stores["name"] = json_body["name"]
+    store_id = uuid.uuid4().hex
+    store_name = {"id" : store_id, "name" : json_body["name"]}
+    stores[store_id] = store_name
     return stores
 
-@app.put("/update_store")
-def update_store():
+@app.put("/update_store/<id>")
+def update_store(id):
     json_body = request.get_json()
-    
+    stores[id]["name"] = json_body["name"]
+    return stores[id]
 
+@app.delete("/delete_store/<id>")
+def delete_store(id):
+    if id in stores:
+        stores.pop(id)
+        return ("deleted")
+    else:
+        return ("store not found")
 
 if __name__ == "__main__":
     app.run(debug=True)
